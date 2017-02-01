98d5f186e53fd4a1b6c84cb6220bfa7f7cc1380a
Anurag kushwaha <kanurag078@gmail.com>
2017-02-02 00:00:41 +0530

    Delete index.js

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import MovieReducer from './movieReducer';
-
-export default combineReducers({
-    list: MovieReducer
-});
-
-//export default list : 'Janet';
